ad26dbaeb0d0f54a88ee0dfbe37f514075c2e81e
Sam Park <sampark18@gmail.com>
2016-09-09 12:08:28 -0700

    Pre-lunch commit

---
+++ db/migrate/20160909190637_create_requests.rb
@@ -0,0 +1,9 @@
+class CreateRequests < ActiveRecord::Migration[5.0]
+  def change
+    create_table :requests do |t|
+      t.string : # PUT DROP DOWN HERE? FOR INDIVIDUAL REQUEST OR REGULAR REQUEST?
+      t.timestamps
+      t.integer :business_id
+    end
+  end
+end
