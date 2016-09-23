c55a5455bfd512a6ecc9d68c9965d371973cf890
Sam Park <sampark18@gmail.com>
2016-09-23 09:39:06 -0700

    Seed database with a few businesses and delete turbolinks which was causing AJAX functionality to create multiple forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 businesses = [
-  ['Pok Pok', 'Restaurant', '']
-
+  ['Pok Pok', 'Restaurant', '3226 SE Division St', 'Andy Ricker', '1', '(503) 232-1387', 1],
+  ['Pip\'s Doughnuts and Chai', 'Donut Shop', '4759 NE Fremont St', 'Jamie Snell', '2', '(503) 206-8692', 2],
+  ['Dove Vivi', 'Restaurant', '2727 NE Glisan St', 'Andy Ricker', '3', '(503) 239-4444', 3],
+  ['Nong\'s Khao Man Gai', 'Restaurant', '609 SE Ankeny St', 'Nong', '4', '(503) 740-2907', 4],
+  ['New Seasons Market Williams', 'Grocery Store', '3445 N Williams Ave', 'Mr. New', '5', '(503) 528-2888', 5],
+  ['Ecliptic Brewing', 'Brewery', '825 N Cook St', 'John Hop', '6', '(503) 265-8002', 6]
 ]
+
+businesses.each do |biz_name, biz_type, biz_address, biz_contact, biz_number, biz_phone, user_id|
+  Business.create( biz_name: biz_name, biz_type: biz_type, biz_address: biz_address, biz_contact: biz_contact, biz_number: biz_number, biz_phone: biz_phone, user_id: user_id)
+end
